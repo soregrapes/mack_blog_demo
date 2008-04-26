5883cd23bfa0a22a0d0299805430c2b26fb4b8d8
Mark Bates <mark@mackframework.com>
2008-04-25 21:21:57 -0400

    More work on file uploads

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,13 +7,11 @@ class Post
   property :created_at, :datetime
   property :updated_at, :datetime
   
-  has_many :uploaded_files, :conditions => ["klass_type = 'post'"], :foreign_key => :klass_id
+  has_many :uploaded_files, :conditions => ["klass_type = 'post'"], :foreign_key => :klass_id, :dependent => :destroy
   
   validates_presence_of :title
   validates_presence_of :email
   validates_presence_of :body
-  
-  before_destroy :delete_uploaded_files
   
   def add_uploaded_file(mack_file)
     if mack_file
@@ -21,18 +19,8 @@ class Post
                                  :klass_id => self.id, 
                                  :original_file_name => mack_file.file_name, 
                                  :file_type => File.extname(mack_file.file_name))
-      mack_file.save_to([UPLOAD_PATH, file.file_path])
+      mack_file.save_to([UploadedFile::UPLOAD_PATH, file.file_path])
     end
   end
-  
-  private
-  def delete_uploaded_files
-    self.uploaded_files.each do |file|
-      FileUtils.rm(File.join(UPLOAD_PATH, file.file_path))
-      file.destroy!
-    end
-  end
-  
-  UPLOAD_PATH = File.join(MACK_PUBLIC, "uploads")
   
 end
--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -12,8 +12,17 @@ class UploadedFile
   validates_presence_of :original_file_name
   validates_presence_of :file_type
   
+  before_destroy :delete_file
+  
   def file_path
     "#{self.id}#{self.file_type}"
+  end
+  
+  UPLOAD_PATH = File.join(MACK_PUBLIC, "uploads")
+  
+  private
+  def delete_file
+    FileUtils.rm(File.join(UPLOAD_PATH, self.file_path))
   end
   
 end
