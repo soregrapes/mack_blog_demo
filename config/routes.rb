977400157cebd812eb7ddf8c2b3fc47739c9f023
Mark Bates <mark@mackframework.com>
2008-05-07 17:06:57 -0400

    rejiggered routes to work with with_options

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Mack::Routes.build do |r|
 
   r.resource :uppity_files # Added by rake generate:scaffold name=uppity_file
-
-  r.total_posts "/posts/total_posts", :controller => :posts, :action => :total_posts
-
+  
+  r.with_options(:controller => :posts) do |map|
+    map.total_posts "/posts/total_posts", :action => :total_posts
+    map.home_page "/"
+  end
+  
   r.resource :posts # Added by rake generate:scaffold name=post
   
-  
   r.say_hi "/say_hi", :controller => :default, :action => :say_hi
-  
-  r.home_page "/", :controller => :posts, :action => :index
   
   r.defaults
   
