18d169c20b694fa59a04ca5f1cbe99a732f8640b
Mark Bates <mark@markbates.com>
2008-05-19 15:14:53 -0400

    Added a render pdf plugin.

--- config/initializers/gems.rb
+++ config/initializers/gems.rb
@@ -10,4 +10,5 @@ require_gems do |gem|
   # gem.add :termios
   # gem.add :rubyzip, :source => "http://gems.rubyforge.org"
   gem.add :markaby, :version => "0.5.0", :libs => :markaby
+  gem.add "pdf-writer", :version => "1.1.8", :libs => "pdf/writer"
 end
