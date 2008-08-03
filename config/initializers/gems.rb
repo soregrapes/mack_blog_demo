0aa06b4dd0c191c7d2902cf8383a01cb65c8abb5
Mark Bates <mark@mackframework.com>
2008-08-03 15:53:06 -0400

    Updated to use mack-data_factory and mack-pdf_writer

--- config/initializers/gems.rb
+++ config/initializers/gems.rb
@@ -10,7 +10,9 @@ require_gems do |gem|
   # gem.add :termios
   # # gem.add :rubyzip, :source => "http://gems.rubyforge.org"
   # gem.add :markaby, :version => "0.5.0", :libs => :markaby
-  gem.add "mack-data_mapper", :version => "0.6.0.100", :libs => "mack-data_mapper"
+  gem.add "mack-data_mapper", :libs => "mack-data_mapper"
   gem.add "mack-paths", :libs => "mack-paths"
+  gem.add "mack-pdf_writer", :libs => "mack-pdf_writer"
+  gem.add "mack-data_factory", :libs => "mack-data_factory"
   gem.add "pdf-writer", :version => "1.1.8", :libs => "pdf/writer"
 end
